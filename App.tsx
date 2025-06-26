4effb3250eaa7b9053b19a403d06d222ad72b44d
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-06-26 17:48:31 -0300

    feat: updated app

--- App.tsx
+++ App.tsx
@@ -1,28 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
+import { lightTheme } from './src/constants/theme';
+import { AppNavigator } from './src/navigation/app-navigator';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <ThemeProvider theme={lightTheme}>
+      <AppNavigator />
+    </ThemeProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+};
 
 export default App;
